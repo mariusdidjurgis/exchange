1fc7ebf338bb95c5442c3e7825d2c42396755684
marius didjurgis <marius.didjurgis@gmail.com>
2021-05-08 17:15:27 +0300

    new project

---
+++ Exchange.Tests/ProgramTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exchange.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Main_WithEmptyParameters_ShouldPrintUsage()
+        {
+            var aa = true;
+            Assert.IsTrue(aa);
+        }
+    }
+}
